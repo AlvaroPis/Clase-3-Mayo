81fb73512280b6c0bbdce9dbdc321594df8a7c81
Alvaro Pistelli <alvaropistelli@outlook.com>
2022-05-03 10:05:36 -0300

    Subida

---
+++ Clase35/Clase35/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase35
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox1.Text = "1000";
+                textBox2.Text = "0";
+                textBox3.Text = (int.Parse(textBox1.Text) / int.Parse(textBox2.Text)).ToString();
+
+            }
+            catch (DivideByZeroException) when (int.Parse(textBox2.Text) == 0) { textBox3.Text = "Text Box 2 = 0"; }
+            catch (DivideByZeroException) when (int.Parse(textBox1.Text) == 0) { textBox3.Text = "Text Box 1 = 0"; }
+            finally { textBox4.Text = "Funciona"; }
+            
+        } 
+    }
+}
---
+++ Clase352/Clase352/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase352
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                float saldo = 1000;
+                textBox1.Text = "300";
+                textBox2.Text = "4";
+
+            if ((int.Parse(textBox1.Text) * int.Parse(textBox2.Text)) > saldo)
+            {
+                textBox3.Text = "No alcanza el saldo";
+            }
+            else { 
+            }
+                
+            
+                
+        }
+    }
+}
